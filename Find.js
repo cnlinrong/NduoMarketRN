8f31b3395edf9653298c813df34f54f6bb8b5b7e
zhuyuliang <zyl3848ea@gmail.com>
2017-01-10 18:12:18 +0800

    oops

--- Find.js
+++ Find.js
@@ -12,6 +12,7 @@ import AppTableCell from './AppTableCell.js';
 
 const APP_LIST_URL = 'http://market3.nduoa.com/?actionid=205&cardid=281776&mt=4&sv=5.2&osv=4.4.2&cpu=armeabi-v7a,armeabi&rslt=720*1280&gpu=&imei=359209027536683&imsi=460005907323770&nt=10&dm=H30-U10&lan=zh-CHT&chl=nduo&cuid=1CFFD9B38A73B154F7235CAD8FCCA5FC%7C386635720902953&tz=GMT%2B08%3A00&apilevel=19&pid=2&sid=abd0428c32b30013bd6d636327da4923&sign=a86115600cade1ec3446562f0ab9ccd5';
 const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+const firstLoad = true;
 
 export default class Find extends Component {
 
@@ -28,14 +29,23 @@ export default class Find extends Component {
     this.getAppList();
   }
 
+  componentDidUpdate() {
+    this.setState({
+      refreshing: false,
+    });
+  }
+
   getAppList = () => {
     fetch(APP_LIST_URL)
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
-          refreshing: false,
+          refreshing: firstLoad ? false : true,
           dataSource: dataSource.cloneWithRows(responseJson.ResponseObject.items)
         });
+        if (firstLoad) {
+          firstLoad = false;
+        }
         return responseJson.ResponseObject.items;
       })
       .catch((error) => {
