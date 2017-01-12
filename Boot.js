942e0ef2d4a7f21303f750824f596f6269f1eca0
cnlinrong <cnlinrong@gmail.com>
2017-01-12 14:11:10 +0800

    Modify page Boot.

--- Boot.js
+++ Boot.js
@@ -4,8 +4,13 @@ import {
   StyleSheet,
   Text,
   View,
-  StatusBar
+  StatusBar,
+  Image
 } from 'react-native';
+
+const LOADING_LOGO = require('./img/loading_logo.png');
+const LOADING_TITLE = require('./img/loading_title.png');
+const LOADING_TIP = require('./img/loading_tip.png');
 
 export default class Boot extends Component {
 
@@ -13,7 +18,15 @@ export default class Boot extends Component {
     return (
       <View style={styles.container}>
         <StatusBar backgroundColor="#ffffff" translucent={true} />
-        <Text style={styles.loading}>N多应用市场</Text>
+        <View style={styles.topContainer}>
+          <Image source={LOADING_LOGO} style={styles.loading_logo} />
+        </View>
+        <View style={styles.middleContainer}>
+          <Text style={styles.loading_title}>N多市场</Text>
+        </View>
+        <View style={styles.bottomContainer}>
+          <Text style={styles.loading_tip}>Copyright © nduo.cn</Text>
+        </View>
       </View>
     );
   }
@@ -23,13 +36,31 @@ export default class Boot extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fff',
   },
-  loading: {
+  loading_logo: {
+    height: 79,
+    width: 79
+  },
+  loading_title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    color: '#000',
+  },
+  loading_tip: {
+    fontSize: 15,
+    color: '#000'
+  },
+  topContainer: {
+    marginTop: 100,
+    alignItems: 'center'
+  },
+  middleContainer: {
+    marginTop: 50,
+    alignItems: 'center',
+    flex: 1
+  },
+  bottomContainer: {
+    alignItems: 'center',
+    marginBottom: 50,
   }
 });
